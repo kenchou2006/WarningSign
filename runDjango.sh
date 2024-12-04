c74822993ebb40af5ea45015ecf9bb2a4e145055
周恭煥 <kenchou2006@gmail.com>
2024-12-04 14:23:27 +0800

    Fix wsgi

--- runDjango.sh
+++ runDjango.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nohup sudo python3 /home/kenchou2006/WarningSign\ 2.0/server/manage.py runserver 0.0.0.0:80
+#nohup sudo python3 /home/kenchou2006/WarningSign\ 2.0/server/manage.py runserver 0.0.0.0:80
+sudo gunicorn --bind 0.0.0.0:443 --certfile=server.crt --keyfile=server.key server.wsgi
